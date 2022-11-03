3b21a1d439c7b9294ca4db91151de564b328e500
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-11-03 23:07:44 +0600

    module - 68

--- src/Loaders/productAndCartLoaders.js
+++ src/Loaders/productAndCartLoaders.js
@@ -2,8 +2,8 @@ import { getStoredCart } from "../utilities/fakedb";
 
 export const productAndCartLoaders = async () => {
   // get Products
-  const productsData = await fetch("products.json");
-  const products = await productsData.json();
+  const productsData = await fetch("http://localhost:5000/products");
+  const { products } = await productsData.json();
 
   // get cart
   const savedCart = getStoredCart();
@@ -11,7 +11,7 @@ export const productAndCartLoaders = async () => {
   //   console.log(savedCart);
   for (const id in savedCart) {
     // console.log(id);
-    const addedProduct = products.find((product) => product.id === id);
+    const addedProduct = products.find((product) => product._id === id);
     // console.log(addedProduct);
     if (addedProduct) {
       const quantity = savedCart[id];
